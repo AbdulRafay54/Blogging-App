19487a58410734c1d8a6f496d6c1d2dbbb37bf9f
AbdulRafay54 <rafayabdul7817@gmail.com>
2024-10-04 18:00:56 +0500

    Working

--- src/components/userblogs.jsx
+++ src/components/userblogs.jsx
@@ -1,8 +1,8 @@
-// UserBlogs.jsx
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../Firebase/firebasemethods";
 import { collection, query, where, getDocs } from "firebase/firestore";
+import { BallTriangle } from "react-loader-spinner";
 
 function UserBlogs() {
   const { userName } = useParams();
@@ -32,23 +32,34 @@ function UserBlogs() {
   }, [userName]);
 
   return (
-    <div className="min-h-screen bg-gray-100 p-8">
+    <div className="min-h-screen bg-gray-900 p-8 flex flex-col items-center justify-center">
       {loading ? (
-        <div className="absolute top-0 flex justify-center items-center w-full h-full">
-          <p className="text-xl font-semibold text-gray-600">Loading...</p>
-        </div>
+        <div className="flex justify-center items-center w-full h-full">
+        <BallTriangle
+          height={100}
+          width={100}
+          radius={5}
+          color="#4fa94d"
+          ariaLabel="ball-triangle-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+        />
+      </div>
       ) : (
         <>
           {blogs.length > 0 ? (
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-10">
+            <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-10">
               {blogs.map((item) => (
                 <div
                   key={item.id}
-                  className="bg-white shadow-lg rounded-lg p-6 transition-shadow duration-300 transform hover:scale-105"
+                  className="bg-gray-800 shadow-lg rounded-lg p-6 transition-transform duration-300 hover:scale-105 transform flex flex-col justify-between"
                 >
-                  <h2 className="text-2xl font-bold text-center text-gray-800 mb-2">{item.title}</h2>
-                  <p className="text-gray-600 text-center mb-4">{item.description}</p>
-                  <p className="text-sm text-center font-medium text-gray-500">By {item.userName}</p>
+                  <div>
+                    <h2 className="text-2xl font-bold text-center text-teal-400 mb-2">{item.title}</h2>
+                    <p className="text-white font-semibold text-center mb-4">{item.description}</p>
+                    <p className="text-md text-center font-medium text-sky-400">By {item.userName}</p>
+                  </div>
                 </div>
               ))}
             </div>
